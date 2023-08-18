336c822e5ec8b381399b91a91f8bbdf4d15b3237
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-08-19 00:55:04 +0300

    feat: Add log, sum and factorial funcs

--- src/examples/HelloWorld/main.go
+++ src/examples/HelloWorld/main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt" // The input/output (I/O) package
 
-func main(){
-	fmt.Println("Hello World!")
+func main() {
+	log("Hello World!")
+
+	var summary int = sum(5, 6)
+	fmt.Printf("5+6 Sum: %v\n", summary)
+	fmt.Printf("5 Factorial: %v\n", factorial(5))
+}
+
+// I created a log function just like the NodeJS's console.log
+func log(message string) {
+	fmt.Printf("%v\n", message)
+}
+
+func sum(firstNumber int, secondNumber int) int {
+	return firstNumber + secondNumber
+}
+
+func factorial(number int) int {
+	if number == 0 {
+		return 1
+	}
+
+	return number * factorial(number-1)
 }
